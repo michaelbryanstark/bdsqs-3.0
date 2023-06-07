1f2f2610cd016b16dd9cbeee1290242395cfd488
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2023-06-07 03:22:52 -0400

    added all components to app.js router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Sidebar from './Components/Sidebar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './Components/Home';
+import Illustrations from './Components/Illustrations';
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Sidebar />
           <Routes>
             <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/illustrations' element={<Illustrations />}></Route>
+            <Route exact path='/projects' element={<Projects />}></Route>
+            <Route exact path='/contact' element={<Contact />}></Route>
           </Routes>
       </div>
     </Router>
---
+++ src/Components/Illustrations.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const Illustrations = () => {
+    return (
+        <div>
+            <h1>
+                Illustrations
+            </h1>
+        </div>
+    )
+};
+
+export default Illustrations;
